2b80766906adb09a260c8534ebef7ccdabaec480
steve0079 <49959235+steve0079@users.noreply.github.com>
2019-09-12 16:54:05 -0700

    Updates

---
+++ precache-manifest.40feb325d9a4911ba40187c2f77def6d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "218c9e401c5c507c779ca63a09c34811",
+    "url": "/hackernews-clone/index.html"
+  },
+  {
+    "revision": "098cb6968251c006a5cf",
+    "url": "/hackernews-clone/static/css/main.7ecce42f.chunk.css"
+  },
+  {
+    "revision": "3b8be6909a3be861ca82",
+    "url": "/hackernews-clone/static/js/2.63d3ad87.chunk.js"
+  },
+  {
+    "revision": "098cb6968251c006a5cf",
+    "url": "/hackernews-clone/static/js/main.3b3b9aef.chunk.js"
+  },
+  {
+    "revision": "ec181592fe49ade8519a",
+    "url": "/hackernews-clone/static/js/runtime~main.98d6bc30.js"
+  }
+]);
